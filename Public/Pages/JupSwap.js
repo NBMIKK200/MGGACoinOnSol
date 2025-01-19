7059705f6070bb5b29de9c5169eec75cfbe8aafb
I-Jac <isac.jacobsson03@gmail.com>
2025-01-19 18:13:20 +0100

    1

--- Public/Pages/JupSwap.js
+++ Public/Pages/JupSwap.js
@@ -1,16 +1,15 @@
-window.Jupiter.init({
-    displayMode: "integrated",
-    integratedTargetId: "integrated-terminal",
-    endpoint: "https://misty-thrilling-scion.solana-mainnet.quiknode.pro/cf8404eb59e4ff88ff2ef1904ea16e8de1de0135/",
-    defaultExplorer: "Solscan",
-    formProps: {
-      fixedOutputMint: true,
-      swapMode: "ExactIn",
-      initialAmount: "100000000",
-      initialOutputMint: "FoyZoKXj8LxH29rND5AH5QcqGcH7FqeD8kjmN9zmpump",
-      initialSlippageBps: 5,
-    },
+document.addEventListener('DOMContentLoaded', () => {
+  window.Jupiter.init({
+      displayMode: "integrated",
+      integratedTargetId: "integrated-terminal",
+      endpoint: "https://misty-thrilling-scion.solana-mainnet.quiknode.pro/cf8404eb59e4ff88ff2ef1904ea16e8de1de0135/",
+      defaultExplorer: "Solscan",
+      formProps: {
+          fixedOutputMint: true,
+          swapMode: "ExactIn",
+          initialAmount: "100000000",
+          initialOutputMint: "FoyZoKXj8LxH29rND5AH5QcqGcH7FqeD8kjmN9zmpump",
+          initialSlippageBps: 5,
+      },
   });
-
-
-  
+});
